3a967b6dd7ddfbeea048c6a31fdf515b4e549e41
Welliton Matos <114311463+Welliton-Matos@users.noreply.github.com>
2024-06-04 13:03:34 -0300

    Atualização com a mensagem final ao cliente.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+       
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    
+        System.out.println("Por favor, digite o seu nome");
+        String nome = scanner.next();
+        
+        System.out.println("Digite seu sobrenome");
+        String agencia = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int numero = scanner.nextInt();
+        
+        System.out.println("Digite sua altura");
+        double saldo = scanner.nextDouble();
+
+    
+        System.out.println("Olá" nome);
+        
+        
     }
 }
