c029eca014d17721e39574709b64ac475e4a1597
Welliton Matos <114311463+Welliton-Matos@users.noreply.github.com>
2024-06-04 13:08:45 -0300

    Correção de nome de variável

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) {
 
-       
-        //criando o objeto scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
     
-        System.out.println("Por favor, digite o seu nome");
+        System.out.println("Por favor, digite o seu nome conforme documento oficial!");
         String nome = scanner.next();
+
+        System.out.println("Por favor, digite o seu sobrenome!");
+        String sobrenome = scanner.next();
         
-        System.out.println("Digite seu sobrenome");
+        System.out.println("Digite o número da sua agência!");
         String agencia = scanner.next();
 
-        System.out.println("Digite sua idade");
+        System.out.println("Digite o número da sua conta!");
         int numero = scanner.nextInt();
         
-        System.out.println("Digite sua altura");
+        System.out.println("Digite o valor do saque");
         double saldo = scanner.nextDouble();
 
-    
-        System.out.println("Olá" nome);
-        
-        
+        System.out.println("Ola" + " " + nome + " " + sobrenome + "," + " " + "obrigado por criar uma conta em nosso banco!");
+        System.out.println("A sua agência é a" + " " + agencia + "," + " " + "conta nº" + " " + numero + " " + "e o seu saldo de" + " " + saldo + " " + "já está disponível para saque!");
+
     }
 }
